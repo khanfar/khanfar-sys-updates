7ace556ca52a13fa07f13bba14cecbafdfd35f50
khanfar <khanfar@github.com>
2024-12-05 21:41:34 +0200

    Reset to version 1.0.1

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
 """Version information."""
 
 def get_version():
-    """Get the current version of the application."""
+    """Get the current version."""
     return "1.0.1"
 
 __version__ = get_version()
